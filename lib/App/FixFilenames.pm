4f465b6a84b49c6c780e9fc6695e2003431e0d77
Thomas Klausner <domm@cpan.org>
2008-11-14 21:23:58 +0100

    initial commit

---
+++ lib/App/FixFilenames.pm
@@ -0,0 +1,34 @@
+package App::FixFilenames;
+
+use strict;
+use warnings;
+use 5.010;
+use version; our $VERSION = version->new('0.01');
+
+1;
+__END__
+
+=head1 NAME
+
+App::FixFilenames -
+
+=head1 SYNOPSIS
+
+  use App::FixFilenames;
+
+=head1 DESCRIPTION
+
+App::FixFilenames is
+
+=head1 AUTHOR
+
+Thomas Klausner E<lt>domm {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
