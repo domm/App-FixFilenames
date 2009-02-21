57e401c6f0fdcfb253c9eadf1a4382766e4af827
Thomas Klausner <domm@cpan.org>
2009-02-21 01:56:12 +0100

    implemented command "gogear"

--- lib/App/FixFilenames/Command/Gogear.pm
+++ lib/App/FixFilenames/Command/Gogear.pm
@@ -7,24 +7,32 @@ use 5.010;
 use App::FixFilenames -command;
 use base 'App::FixFilenames';
 use MP3::Info ();
+use File::Spec::Functions;
+use Data::Dumper;
 
 sub opt_spec {
+    my $self = shift;
+    
     return (
-        [ 'gogear=s',    'mount point of gogear disk' ],
+        [ 'gogear=s',    'mount point of gogear disk', { default => '/mnt/gogear' }  ],
+        [ 'type=s@',    'file type (default mp3)', { default => ['mp3'] }  ],
     );
 }
 
 sub run {
     my ( $self, $opt, $args ) = @_;
-
-    $self->findfiles;
+    
+    $self->findfiles($opt);
 
     foreach my $path ( @{ $self->files } ) {
         say "processing $path" if $self->verbose > 1;
         my ( $dir, $file, $ext ) = $self->splitfilepath($path);
-        $file =~ s/\D//g;
-        my $new = sprintf( "%02d", $file );
-        #$self->rename_file( $path, $dir, $new . '.' . $ext );
+        
+        my $info = MP3::Info->new($path);
+        my $album = $self->_safe_filename($info->album // 'unkown');
+        
+        $file = $album .'_'.$file .'.'.$ext;
+        $self->copy_file( $path, catdir($opt->{gogear},qw(_system media audio)), $file );
     }
 
     $self->report;
@@ -35,11 +43,11 @@ __END__
 
 =head1 NAME
 
-App::FixFilenames::Command::Cut2Tracknum - remove everything before what looks like a tracknum
+App::FixFilenames::Command::Gogear - copy & rename files to a gogear mp3 player
 
 =head1 SYNOPSIS
 
-  fixfilenames.pl cut2tracknum --type mp3
+  fixfilenames.pl gogear
 
 =head1 DESCRIPTION
 
