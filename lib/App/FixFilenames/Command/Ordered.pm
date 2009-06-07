5453174f7ccc69869dd05d4c0f86afa1521b2a50
Thomas Klausner <domm@cpan.org>
2009-06-07 20:29:15 +0200

    added --exif and --padding options to ordered

--- lib/App/FixFilenames/Command/Ordered.pm
+++ lib/App/FixFilenames/Command/Ordered.pm
@@ -6,13 +6,18 @@ use 5.010;
 
 use App::FixFilenames -command;
 use base 'App::FixFilenames';
-
+use DateTime::Format::Strptime;
+use Image::ExifTool;
 use File::stat;
+
+my $dateparser = DateTime::Format::Strptime->new(pattern=>'%Y:%m:%d %H:%M:%S');
 
 sub opt_spec {
     return (
         [ 'base=s',    'base name for new files' ],
         [ 'start=s',   'start counter' ],
+        [ 'padding=s',   'number of zeros to pad' ],
+        [ 'exif',        'use exif date instead of file mtime'],
     );
 }
 
@@ -22,13 +27,28 @@ sub run {
     $self->findfiles;
 
     my %info;
-    foreach my $path ( @{ $self->files } ) {
-        my $stat = stat($path);
-        $info{$path}=$stat->mtime;
+    
+    if ($opt->{exif}) {
+        foreach my $path ( @{ $self->files } ) {
+            my $exif = Image::ExifTool->new;
+            $exif->ExtractInfo($path);
+            my $created = $dateparser->parse_datetime($exif->GetValue('CreateDate'))->epoch;
+            $info{$path}=$created;
+            utime($created,$created,$path);
+        }
+    }
+    else {
+        foreach my $path ( @{ $self->files } ) {
+            my $stat = stat($path);
+            $info{$path}=$stat->mtime;
+        }
     }
     
     my $format = $opt->{base} ? $opt->{base}.'_' : '';
-    $format.="%0".length(scalar keys %info)."d";
+    my $zeros = $opt->{padding} ||0;
+    my $length = length(scalar keys %info);
+    $zeros = $length if $zeros < $length;
+    $format.="%0".$zeros."d";
     
     my $count = $opt->{start} // 1;
     foreach my $path ( sort { $info{$a} <=> $info{$b} } keys %info ) {
